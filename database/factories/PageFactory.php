a32e1e66da3bdb2283672032056ef3b5e134f4a0
Jakob Buis <jakob@jakobbuis.nl>
2017-12-10 23:33:30 +0100

    much more realistic db seeding

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -5,14 +5,12 @@ use App\Page;
 use Faker\Generator as Faker;
 
 $factory->define(Page::class, function (Faker $faker) {
-    $notebook = factory(Notebook::class)->create();
-    $start = $faker->numberBetween(0, $notebook->page_count);
-    $end = $faker->numberBetween($start, $notebook->page_count);
-
     return [
-        'notebook_id' => $notebook->id,
-        'start_number' => $start,
-        'end_number' => $end,
+        'notebook_id' => function() {
+            return factory(Notebook::class)->create()->id;
+        },
+        'start_number' => $faker->numberBetween(0, 30),
+        'end_number' => $faker->numberBetween(31, 42),
         'description' => $faker->text,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $tags = factory(\App\Tag::class, 5)->create();
+
+        // Create 5 notebooks, each with 3 pages
+        // each linked to 2 randomly selected tags
+        $notebooks = factory(\App\Notebook::class, 5)->create();
+        $notebooks->each(function($notebook) use ($tags) {
+
+            $pages = factory(\App\Page::class, 3)->create([
+                'notebook_id' => $notebook->id,
+            ]);
+
+            $pages->each(function($page) use ($tags) {
+                $page->tags()->attach($tags->random(2));
+            });
+        });
     }
 }
