46959c0510127be14422694b79d646a2c58d5e95
Jakob Buis <jakob@jakobbuis.nl>
2017-12-17 12:04:38 +0100

    more varied descriptions in PageFactory

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -5,12 +5,17 @@ use App\Page;
 use Faker\Generator as Faker;
 
 $factory->define(Page::class, function (Faker $faker) {
+    // Create varied descriptions
+    $wordCount = $faker->numberBetween(2, 10);
+    $text = $faker->words($wordCount, true);
+    $description = $faker->randomElement([null, $text]);
+
     return [
         'notebook_id' => function() {
             return factory(Notebook::class)->create()->id;
         },
         'start_number' => $faker->numberBetween(0, 30),
         'end_number' => $faker->numberBetween(31, 42),
-        'description' => $faker->text,
+        'description' => $description,
     ];
 });
