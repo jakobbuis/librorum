0de8de400c4ca79f4c06dd11f2eb5764ece1eafc
Jakob Buis <jakob@jakobbuis.nl>
2017-12-24 19:20:28 +0100

    app name goes to home

--- resources/assets/js/components/LayoutHeader.vue
+++ resources/assets/js/components/LayoutHeader.vue
@@ -1,7 +1,9 @@
 <template>
     <header class="mdl-layout__header mdl-layout__header--waterfall">
         <div class="mdl-layout__header-row">
-            <span class="mdl-layout-title">Librorum</span>
+            <router-link to="/">
+                <span class="mdl-layout-title">Librorum</span>
+            </router-link>
 
             <div class="mdl-layout-spacer"></div>
         </div>
@@ -11,5 +13,11 @@
 <style scoped>
 .mdl-layout__header-row {
     padding-left: 16px;
+}
+
+a {
+    color: white;
+    text-decoration: none;
+    font-weight: normal;
 }
 </style>
