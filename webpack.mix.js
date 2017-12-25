a96ce1945a74dd5a33ff3a46c658e4da7eda0c2b
Jakob Buis <jakob@jakobbuis.nl>
2017-12-25 22:34:37 +0100

    using source maps

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ let mix = require('laravel-mix');
 
 mix.disableSuccessNotifications();
 
-mix.js('resources/assets/js/app.js', 'public/js').version();
-mix.sass('resources/assets/sass/app.scss', 'public/css').version();
+mix.js('resources/assets/js/app.js', 'public/js').version().sourceMaps();
+mix.sass('resources/assets/sass/app.scss', 'public/css').version().sourceMaps();
