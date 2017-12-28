c3e49f76613e65915ff3be5a8f524e80807ebb73
Jakob Buis <jakob@jakobbuis.nl>
2017-12-28 22:53:25 +0100

    fix position fixed of FAB by moving it outside of the app

--- resources/assets/js/components/Tags.vue
+++ resources/assets/js/components/Tags.vue
@@ -1,38 +1,40 @@
 <template>
-    <md-app>
-        <md-app-toolbar class="md-primary">
-            <div class="md-toolbar-section-start">
-                <span class="md-title">Librorum</span>
-            </div>
+    <div>
+        <md-app>
+            <md-app-toolbar class="md-primary">
+                <div class="md-toolbar-section-start">
+                    <span class="md-title">Librorum</span>
+                </div>
 
-            <div class="md-toolbar-section-end">
-                <md-field md-clearable v-if="searching">
-                    <label>Search</label>
-                    <md-input ref="searchField" v-model="filter"></md-input>
-                </md-field>
-                <md-button class="md-icon-button" @click="searching = true">
-                    <md-icon>search</md-icon>
-                </md-button>
-            </div>
-        </md-app-toolbar>
+                <div class="md-toolbar-section-end">
+                    <md-field md-clearable v-if="searching">
+                        <label>Search</label>
+                        <md-input ref="searchField" v-model="filter"></md-input>
+                    </md-field>
+                    <md-button class="md-icon-button" @click="searching = true">
+                        <md-icon>search</md-icon>
+                    </md-button>
+                </div>
+            </md-app-toolbar>
 
-        <md-app-content>
-            <md-list class="md-triple-line">
-                <transition-group tag="div" name="tag-complete" :duration="500">
-                    <template v-for="(tag, index) in displayedTags">
-                        <tag :tag="tag" :key="tag.tag" v-model="displayedTags[index]"></tag>
-                        <md-divider :key="index" v-if="index !== displayedTags.length - 1" />
-                    </template>
-                </transition-group>
-            </md-list>
+            <md-app-content>
+                <md-list class="md-triple-line">
+                    <transition-group tag="div" name="tag-complete" :duration="500">
+                        <template v-for="(tag, index) in displayedTags">
+                            <tag :tag="tag" :key="tag.tag" v-model="displayedTags[index]"></tag>
+                            <md-divider :key="index" v-if="index !== displayedTags.length - 1" />
+                        </template>
+                    </transition-group>
+                </md-list>
+            </md-app-content>
+        </md-app>
 
-            <router-link to="/add-page">
-                <md-button class="md-fab md-primary">
-                    <md-icon>add</md-icon>
-                </md-button>
-            </router-link>
-        </md-app-content>
-    </md-app>
+        <router-link to="/add-page">
+            <md-button class="md-fab md-primary">
+                <md-icon>add</md-icon>
+            </md-button>
+        </router-link>
+    </div>
 </template>
 
 <script>
