ca91cc4bd4506112daabfab638e2a22965f34304
Jakob Buis <jakob@jakobbuis.nl>
2017-12-28 22:18:22 +0100

    add UI to TagDetail

--- resources/assets/js/components/TagDetail.vue
+++ resources/assets/js/components/TagDetail.vue
@@ -1,28 +1,39 @@
 <template>
-    <div>
-        <h1>
-            {{ tag.tag }}
-            <i class="material-icons">{{ starStatus }}</i>
-        </h1>
+     <md-app>
+        <md-app-toolbar class="md-primary">
+            <router-link to="/">
+                <md-button class="md-icon-button">
+                    <md-icon>arrow_back</md-icon>
+                </md-button>
+            </router-link>
+            <span class="md-title">Librorum</span>
+        </md-app-toolbar>
 
-        <md-table>
-            <md-table-row>
-                <md-table-head>Notebook</md-table-head>
-                <md-table-head md-numeric>Pages</md-table-head>
-                <md-table-head>Notes</md-table-head>
-            </md-table-row>
+        <md-app-content>
+            <h1>
+                {{ tag.tag }}
+                <i class="material-icons">{{ starStatus }}</i>
+            </h1>
 
-            <md-table-row v-for="page in tag.pages" :key="page.identifier">
-                <md-table-cell>
-                    <md-chip :style="{'background-color': page.color}">
-                        {{ page.notebook }}
-                    </md-chip>
-                </md-table-cell>
-                <md-table-cell md-numeric>{{ page.start_number }}&#8210;{{ page.end_number }}</md-table-cell>
-                <md-table-cell>{{ page.description }}</md-table-cell>
-            </md-table-row>
-        </md-table>
-    </div>
+            <md-table>
+                <md-table-row>
+                    <md-table-head>Notebook</md-table-head>
+                    <md-table-head md-numeric>Pages</md-table-head>
+                    <md-table-head>Notes</md-table-head>
+                </md-table-row>
+
+                <md-table-row v-for="page in tag.pages" :key="page.identifier">
+                    <md-table-cell>
+                        <md-chip :style="{'background-color': page.color}">
+                            {{ page.notebook }}
+                        </md-chip>
+                    </md-table-cell>
+                    <md-table-cell md-numeric>{{ page.start_number }}&#8210;{{ page.end_number }}</md-table-cell>
+                    <md-table-cell>{{ page.description }}</md-table-cell>
+                </md-table-row>
+            </md-table>
+        </md-app-content>
+    </md-app>
 </template>
 
 <script>
