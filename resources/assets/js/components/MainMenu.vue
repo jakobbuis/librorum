74b6e9eacf6de937067ba179bed692a16df812a8
Jakob Buis <jakob@jakobbuis.nl>
2018-01-02 00:05:30 +0100

    add main menu, and dummy trash component

---
+++ resources/assets/js/components/MainMenu.vue
@@ -0,0 +1,33 @@
+<template>
+    <md-drawer
+        :md-active="visible"
+        @update:mdActive="activeChanged">
+
+        <md-list>
+            <router-link to="/">
+                <md-list-item>
+                    <md-icon>label</md-icon>
+                    <span class="md-list-item-text">Tags</span>
+                </md-list-item>
+            </router-link>
+
+            <router-link to="/trash">
+                <md-list-item>
+                    <md-icon>delete</md-icon>
+                    <span class="md-list-item-text">Trash</span>
+                </md-list-item>
+            </router-link>
+        </md-list>
+      </md-drawer>
+</template>
+
+<script>
+export default {
+    props: ['visible'],
+    methods: {
+        activeChanged(value) {
+            this.$emit('update:visible', value);
+        },
+    },
+};
+</script>
---
+++ resources/assets/js/components/Trash.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <md-app>
+            <md-app-toolbar class="md-primary">
+                <md-button class="md-icon-button" @click="menuVisible = true">
+                    <md-icon>menu</md-icon>
+                </md-button>
+                <span class="md-title">Trash</span>
+            </md-app-toolbar>
+
+            <md-app-content>
+                <h1>Trash here</h1>
+            </md-app-content>
+        </md-app>
+
+        <main-menu :visible.sync="menuVisible"></main-menu>
+    </div>
+</template>
+
+<script>
+import MainMenu from './MainMenu';
+
+export default {
+    components: { MainMenu },
+    data() {
+        return {
+            menuVisible: false, // Show the main navigation menu
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.tag-complete-move {
+  transition: transform 0.5s;
+}
+.md-list {
+    border: 1px solid rgba(#000, .12);
+    padding: 0;
+}
+.md-speed-dial {
+    position: fixed;
+    right: 16px;
+    bottom: 16px;
+    z-index: 2;
+
+    .md-button {
+        background-color: rgb(150, 150, 150);
+    }
+
+    .md-icon {
+        color: white;
+    }
+}
+</style>
