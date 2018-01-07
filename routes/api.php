3020db3abba38752af448f99cc0c78654f401a46
Jakob Buis <jakob@jakobbuis.nl>
2018-01-07 17:01:55 +0100

    add forgotten route to clearing trash

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ Route::apiResource('tags', 'TagsController');
 Route::apiResource('notebooks.tags', 'NotebookTagsController');
 Route::apiResource('pages', 'PagesController');
 Route::apiResource('trash', 'TrashController');
+
+// DELETE /trash destroys all trashed items
+Route::delete('trash', 'TrashController@purge');
