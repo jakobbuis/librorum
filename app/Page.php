2e548bbebce55745f4ee92e19d826c3a8078f952
Jakob Buis <jakob@jakobbuis.nl>
2018-02-11 22:26:10 +0100

    handle null case for page->end_number as it is optional

--- app/Page.php
+++ app/Page.php
@@ -30,7 +30,7 @@ class Page extends Model
      * Format the starting page number with a leading zero if required
      * @return string e.g. 01, 06, 10, 11 or 123
      */
-    public function getStartNumberAttribute(int $value)
+    public function getStartNumberAttribute($value)
     {
         return str_pad((string) $value, 2, '0', STR_PAD_LEFT);
     }
@@ -39,8 +39,12 @@ class Page extends Model
      * Format the ending page number with a leading zero if required
      * @return string e.g. 01, 06, 10, 11 or 123
      */
-    public function getEndNumberAttribute(int $value)
+    public function getEndNumberAttribute($value)
     {
+        if ($value === null) {
+            return null;
+        }
+
         return str_pad((string) $value, 2, '0', STR_PAD_LEFT);
     }
 
